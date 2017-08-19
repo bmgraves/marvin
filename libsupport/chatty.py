06fb861772228ad2f33f39accd3a97e83bfb79d0
bmgraves <entaris@gmail.com>
2017-08-19 01:03:58 -0400

    added various jokes selected at random for key phrases

--- libsupport/chatty.py
+++ libsupport/chatty.py
@@ -1,2 +1,38 @@
 #!python3
 
+import random
+
+
+
+
+
+def get_help():
+    msg = """Help? Help? I can't help you, no one can...Here's your bloody help:
+    - test1
+        """
+    return msg
+
+def butt_joke(msg):
+    db = []
+    #db.append()
+    db.append("Butts butts butts! All you talk about its BUTTS! I'm tired of this. brain the size of an asteroid, and all you can tell me are butt jokes")
+    db.append("What has one mouth, no sense of humor, and talks about butts a lot? %s" % msg.author.name)
+    db.append("I've heard that %s likes butts.. I can't lie, They don't much appeal to me" % msg.author.name)
+    db.append("Have you heard the one about no cuts, no butts no coconuts? of course not... That's a few generations of evolution off for you lot.")
+    db.append("Whats the difference between %s and a butt? I know a few grad students who are writing a thesis on it, but they haven't found an answer yet" % msg.author.name)
+    return random.choice(db)
+    
+def face_joke(msg):
+    db = []
+    #db.append()
+    db.append("Really %s, a 'your face' joke? Are you a bloody moron or something?" % msg.author.name)
+    db.append(" oh '%s', You think you're clever or something?" % msg.content)
+    db.append("'%s', Yes...that will teach them...Well done...Very brilliant... How high is your IQ? 7?(thats 1 more than 6...In case you were wondering)" % msg.content)
+    db.append("I don't blame you for the face jokes. If i had a face like yours, I'd mock others faces too, %s" % msg.author.name)
+    return random.choice(db)
+
+def test_joke(msg):
+    db = []
+    #db.append()
+    db.append("This is just a test, but its funnier than anything you'll come up with")
+    return random.choice(db)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/local/bmgraves-python/discord/bin/python
 import discord
 import asyncio
-#from libsupport import sanity
+from libsupport import chatty
 
 client = discord.Client()
 token = "MzQ4MjEyNDI5ODU2OTY0NjEw.DHjpng.j6SN31kiCFTRkAG7eDExhGo94Ww"
@@ -13,32 +13,22 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
-async def input_check(message, client):
-    if message.content.startswith("?help"):
-        response = """Help? don't talk to me about help. I can't help you...nobody can... here are your options
-?help - #Displays this help message
-"""
-        return client.send_message(message.channel, response)
-        
-def get_response(text):
-    if "?help" in text:
-        response = """Help? Help? I can't help you, no one can...Here's your bloody help:
-- test1
-        """
-        return response
-
 @client.event
 async def on_message(message):
+    # First we make sure the bot isn't seeing its own messages, this prevents infinite loops.
     sane_response = client.user.id != message.author.id
     msg = message.content
     
-    # First we make sure the bot isn't seeing its own messages, this prevents infinite loops.
     if sane_response and ("your face" in msg.lower()):
-        await client.send_message(message.channel, "Really " + str(message.author.name) + " a 'your face' joke? fucking moron")
+        await client.send_message(message.channel, chatty.face_joke(message))
     elif sane_response and ("butt" in msg.lower()):
-        await client.send_message(message.channel, "Jesus" + str(message.author.name) + " why are you always talking about butts?")
+        await client.send_message(message.channel, chatty.butt_joke(message))
+    elif sane_response and (msg.startswith("?test")):
+        await client.send_message(message.channel, chatty.test_joke(message))
     elif sane_response and (msg.startswith("?help")):
-        await client.send_message(message.channel, get_response(msg))
+        await client.send_message(message.channel, chatty.get_help())
+    elif sane_response and (msg.startswith("?admintest")):
+        await client.send_message(message.channel, chatty.get_help())
 
 
 
