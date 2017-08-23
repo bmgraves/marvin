23bb2fc13aa989b9eead9061f3813c21da272a09
Corvious <therion.sheol@gmail.com>
2017-08-23 12:55:25 -0700

    Update chatty.py
    Added dialogue

--- libsupport/chatty.py
+++ libsupport/chatty.py
@@ -69,7 +69,7 @@ def marvin_quote():
     db.append("I think Cathy hates me....")
     db.append(":slight_frown:")
     db.append(":frowning2:")
-    db.append("I like watching you all... James is a bit weird though.")
+    db.append("It's depressing watching all of you... James most of all...")
     #db.append("")
     #db.append("")
     return random.choice(db)
@@ -97,6 +97,7 @@ def mention(msg, boredom):
     "Are you talking to me? I don't believe it. Nobody ever talks to me",
     "How depressing. Nothing better to do with my time then interact with a glorified monkey.",
     "What is it that you want %s? You must be desperate if you're talking to me about it" % msg.author.name,
+    "Are you talking to me %s? Does this mean I have a friend? <:vaultcomp:349942729532375050>" % msg.author.name,
     ]
     if ("life" in msg.content):
         return "Life? Don't talk to me about life"
